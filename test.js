f0050d0eb3f66f682d6986b3c13c3a39f60f9d42
Nenad Vukicevic <vukicevic@gmail.com>
2015-05-13 00:15:26 +0200

    rewrite tests for mocha

--- test.js
+++ test.js
@@ -1,84 +1,132 @@
-var ephem = require('./index');
+var ephem  = require('./index');
 var assert = require('assert');
 
-var e202 = ephem.load('./eph/lnxp1900p2050.202');
-// var e403 = ephem.load('./eph/lnxp2000.403');
-// var e405 = ephem.load('./eph/lnx1900.405');
-// var e430 = ephem.load('./eph/linux_p1550p2650.430');
-var e431 = ephem.load('./eph/lnxm13000p17000.431');
+describe('Ephem Tests', function () {
 
-var time = ephem.julian(new Date("2015-04-21T12:00:00.000Z"));
-var position;
+  before(function () {
+    e202 = ephem.load('./eph/lnxp1900p2050.202');
+    e431 = ephem.load('./eph/lnxm13000p17000.431');
 
-console.log(e431.find(ephem.bodies.MARS, ephem.bodies.PLUTO, time));
-console.log(e202.find(ephem.bodies.MARS, ephem.bodies.PLUTO, time));
+    assert.equal(e202.status(), 0);
+    assert.equal(e431.status(), 0);
 
-position = ephem.radec(e431, ephem.bodies.MERCURY, time)
-console.log(position);
+    time = ephem.julian(new Date("2015-04-21T12:00:00.000Z"));
+  });
 
-assert.equal(position.α[0], 2, 'Mercury R.A. incorrect');
-assert.equal(position.α[1], 41, 'Mercury R.A. incorrect');
-assert.equal(position.α[2], 47.5, 'Mercury R.A. incorrect');
-assert.equal(position.δ[0], 16, 'Mercury Dec. incorrect');
-assert.equal(position.δ[1], 54, 'Mercury Dec. incorrect');
-assert.equal(position.δ[2], 44.3, 'Mercury Dec. incorrect');
+  var e202, e431, time;
 
-position = ephem.radec(e431, ephem.bodies.VENUS, time);
-console.log(position);
+  describe('Position', function () {
 
-assert.equal(position.α[0], 4, 'Venus R.A. incorrect');
-assert.equal(position.α[1], 37, 'Venus R.A. incorrect');
-assert.equal(position.α[2], 27.9, 'Venus R.A. incorrect');
-assert.equal(position.δ[0], 24, 'Venus Dec. incorrect');
-assert.equal(position.δ[1], 4, 'Venus Dec. incorrect');
-assert.equal(position.δ[2], 21.5, 'Venus Dec. incorrect');
+    it('should find Pluto 431', function (done) {
+      assert.ok(e431.find(ephem.bodies.MARS, ephem.bodies.PLUTO, time));
 
-position = ephem.radec(e431, ephem.bodies.MARS, time);
-console.log(position);
+      done();
+    });
 
-assert.equal(position.α[0], 2, 'Mars R.A. incorrect');
-assert.equal(position.α[1], 50, 'Mars R.A. incorrect');
-assert.equal(position.α[2], 11.52, 'Mars R.A. incorrect');
-assert.equal(position.δ[0], 16, 'Mars Dec. incorrect');
-assert.equal(position.δ[1], 26, 'Mars Dec. incorrect');
-assert.equal(position.δ[2], 36.5, 'Mars Dec. incorrect');
+    it('should find Pluto 202', function (done) {
+      assert.ok(e202.find(ephem.bodies.MARS, ephem.bodies.PLUTO, time));
 
-position = ephem.radec(e431, ephem.bodies.JUPITER, time);
-console.log(position);
+      done();
+    });
 
-assert.equal(position.α[0], 9, 'Jupiter R.A. incorrect');
-assert.equal(position.α[1], 1, 'Jupiter R.A. incorrect');
-//assert.equal(position.α[2], 29.03, 'Jupiter R.A. incorrect'); //precision error, off but 0.1", possibly due to Teph
-assert.equal(position.δ[0], 17, 'Jupiter Dec. incorrect');
-assert.equal(position.δ[1], 54, 'Jupiter Dec. incorrect');
-assert.equal(position.δ[2], 54.9, 'Jupiter Dec. incorrect');
+  });
 
-position = ephem.radec(e431, ephem.bodies.SATURN, time);
-console.log(position);
+  describe('Right Ascension, Desclination', function () {
 
-assert.equal(position.α[0], 16, 'Saturn R.A. incorrect');
-assert.equal(position.α[1], 7, 'Saturn R.A. incorrect');
-assert.equal(position.α[2], 58.61, 'Saturn R.A. incorrect');
-assert.equal(position.δ[0], -18, 'Saturn Dec. incorrect');
-assert.equal(position.δ[1], 43, 'Saturn Dec. incorrect');
-assert.equal(position.δ[2], 43.6, 'Saturn Dec. incorrect');
+    it('should find Mercury', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.MERCURY, time)
 
-position = ephem.radec(e431, ephem.bodies.URANUS, time);
-console.log(position);
+      assert.equal(position.α[0], 2, 'Mercury R.A. incorrect');
+      assert.equal(position.α[1], 41, 'Mercury R.A. incorrect');
+      assert.equal(position.α[2], 47.5, 'Mercury R.A. incorrect');
+      assert.equal(position.δ[0], 16, 'Mercury Dec. incorrect');
+      assert.equal(position.δ[1], 54, 'Mercury Dec. incorrect');
+      assert.equal(position.δ[2], 44.3, 'Mercury Dec. incorrect');
 
-assert.equal(position.α[0], 1, 'Uranus R.A. incorrect');
-assert.equal(position.α[1], 3, 'Uranus R.A. incorrect');
-assert.equal(position.α[2], 58.66, 'Uranus R.A. incorrect');
-assert.equal(position.δ[0], 6, 'Uranus Dec. incorrect');
-assert.equal(position.δ[1], 8, 'Uranus Dec. incorrect');
-assert.equal(position.δ[2], 23.7, 'Uranus Dec. incorrect');
+      done();
+    });
 
-position = ephem.radec(e431, ephem.bodies.NEPTUNE, time);
-console.log(position);
+    it('should find Venus', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.VENUS, time);
 
-assert.equal(position.α[0], 22, 'Neptune R.A. incorrect');
-assert.equal(position.α[1], 43, 'Neptune R.A. incorrect');
-assert.equal(position.α[2], 8.86, 'Neptune R.A. incorrect');
-assert.equal(position.δ[0], -8, 'Neptune Dec. incorrect');
-assert.equal(position.δ[1], 55, 'Neptune Dec. incorrect');
-assert.equal(position.δ[2], 44, 'Neptune Dec. incorrect');
+      assert.equal(position.α[0], 4, 'Venus R.A. incorrect');
+      assert.equal(position.α[1], 37, 'Venus R.A. incorrect');
+      assert.equal(position.α[2], 27.9, 'Venus R.A. incorrect');
+      assert.equal(position.δ[0], 24, 'Venus Dec. incorrect');
+      assert.equal(position.δ[1], 4, 'Venus Dec. incorrect');
+      assert.equal(position.δ[2], 21.5, 'Venus Dec. incorrect');
+
+      done();
+    });
+
+    it('should find Mars', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.MARS, time);
+
+      assert.equal(position.α[0], 2, 'Mars R.A. incorrect');
+      assert.equal(position.α[1], 50, 'Mars R.A. incorrect');
+      assert.equal(position.α[2], 11.52, 'Mars R.A. incorrect');
+      assert.equal(position.δ[0], 16, 'Mars Dec. incorrect');
+      assert.equal(position.δ[1], 26, 'Mars Dec. incorrect');
+      assert.equal(position.δ[2], 36.5, 'Mars Dec. incorrect');
+
+      done();
+    });
+
+
+    it('should find Jupiter', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.JUPITER, time);
+
+      assert.equal(position.α[0], 9, 'Jupiter R.A. incorrect');
+      assert.equal(position.α[1], 1, 'Jupiter R.A. incorrect');
+      // assert.equal(position.α[2], 29.03, 'Jupiter R.A. incorrect'); //precision error, off but 0.1", possibly due to Teph
+      assert.equal(position.δ[0], 17, 'Jupiter Dec. incorrect');
+      assert.equal(position.δ[1], 54, 'Jupiter Dec. incorrect');
+      assert.equal(position.δ[2], 54.9, 'Jupiter Dec. incorrect');
+
+      done();
+    });
+
+
+    it('should find Saturn', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.SATURN, time);
+
+      assert.equal(position.α[0], 16, 'Saturn R.A. incorrect');
+      assert.equal(position.α[1], 7, 'Saturn R.A. incorrect');
+      assert.equal(position.α[2], 58.61, 'Saturn R.A. incorrect');
+      assert.equal(position.δ[0], -18, 'Saturn Dec. incorrect');
+      assert.equal(position.δ[1], 43, 'Saturn Dec. incorrect');
+      assert.equal(position.δ[2], 43.6, 'Saturn Dec. incorrect');
+
+      done();
+    });
+
+
+    it('should find Uranus', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.URANUS, time);
+
+      assert.equal(position.α[0], 1, 'Uranus R.A. incorrect');
+      assert.equal(position.α[1], 3, 'Uranus R.A. incorrect');
+      assert.equal(position.α[2], 58.66, 'Uranus R.A. incorrect');
+      assert.equal(position.δ[0], 6, 'Uranus Dec. incorrect');
+      assert.equal(position.δ[1], 8, 'Uranus Dec. incorrect');
+      assert.equal(position.δ[2], 23.7, 'Uranus Dec. incorrect');
+
+      done();
+    });
+
+    it('should find Neptune', function (done) {
+      var position = ephem.radec(e431, ephem.bodies.NEPTUNE, time);
+
+      assert.equal(position.α[0], 22, 'Neptune R.A. incorrect');
+      assert.equal(position.α[1], 43, 'Neptune R.A. incorrect');
+      assert.equal(position.α[2], 8.86, 'Neptune R.A. incorrect');
+      assert.equal(position.δ[0], -8, 'Neptune Dec. incorrect');
+      assert.equal(position.δ[1], 55, 'Neptune Dec. incorrect');
+      assert.equal(position.δ[2], 44, 'Neptune Dec. incorrect');
+
+      done();
+    });
+
+  });
+
+});
