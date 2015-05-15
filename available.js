fccc7ae584c8286c12e29d5a7001d7de1b27c2c6
Nenad Vukicevic <vukicevic@gmail.com>
2015-05-16 00:20:10 +0200

    Test runner

--- available.js
+++ available.js
@@ -1,33 +1,37 @@
-var fs   = require('fs');
+var fs = require('fs');
+var ph = require('path');
 
 var list = {
-
-    'DE102'  : 'lnxm1410p3002.102'
-  , 'DE200'  : 'lnxm1600p2170.200'
-  , 'DE202'  : 'lnxp1900p2050.202'
-  , 'DE403'  : 'lnxp1600p2200.403'
-  , 'DE405'  : 'lnxp1600p2200.405'
-  , 'DE406'  : 'lnxm3000p3000.406'
-  , 'DE410'  : 'lnxp1960p2020.410'
-  , 'DE413'  : 'lnxp1900p2050.413'
-  , 'DE414'  : 'lnxp1600p2200.414'
-  , 'DE418'  : 'lnxp1900p2050.418'
-  , 'DE421'  : 'lnxp1900p2053.421'
-  , 'DE422'  : 'lnxm3000p3000.422'
-  , 'DE423'  : 'lnxp1800p2200.423'
-  , 'DE430'  : 'linux_p1550p2650.430'
-  , 'DE430t' : 'linux_p1550p2650.430t'
-  , 'DE431'  : 'lnxm13000p17000.431'
-
+    '102'  : 'lnxm1410p3002.102'
+  , '200'  : 'lnxm1600p2170.200'
+  , '202'  : 'lnxp1900p2050.202'
+  , '403'  : 'lnxp1600p2200.403'
+  , '405'  : 'lnxp1600p2200.405'
+  , '406'  : 'lnxm3000p3000.406'
+  , '410'  : 'lnxp1960p2020.410'
+  , '413'  : 'lnxp1900p2050.413'
+  , '414'  : 'lnxp1600p2200.414'
+  , '418'  : 'lnxp1900p2050.418'
+  , '421'  : 'lnxp1900p2053.421'
+  , '422'  : 'lnxm3000p3000.422'
+  , '423'  : 'lnxp1800p2200.423'
+  , '430'  : 'linux_p1550p2650.430'
+  , '430t' : 'linux_p1550p2650.430t'
+  , '431'  : 'lnxm13000p17000.431'
 };
 
 function available (path) {
   var files     = fs.readdirSync(path);
-  var available = {};
+  var available = [];
 
   Object.keys(list).forEach(function (de) {
-    if (files.indexOf(list[de]) >= 0)
-      available[de] = list[de];
+    if (files.indexOf(list[de]) >= 0) {
+      available.push({
+          'name' : 'DE' + de
+        , 'file' : path + ph.sep + list[de]
+        , 'test' : files.indexOf('testpo.' + de) >= 0 ? path + ph.sep + 'testpo.' + de : null
+      });
+    }
   });
 
   return available;
--- testrunner.js
+++ testrunner.js
@@ -1,49 +1,113 @@
 var assert = require('assert');
 var reader = require('readline');
 var fs     = require('fs');
+
 var ephem  = require('./index');
+var avail  = require('./available');
 
-var rd    = reader.createInterface({
-    input: fs.createReadStream('./eph/testpo.430t'),
-    output: process.stdout,
-    terminal: false
+var rl = reader.createInterface({
+    'input'  : process.stdin
+  , 'output' : process.stdout
 });
 
-var e430t = ephem.load('./eph/linux_p1550p2650.430t');
+rl.write('Welcome to the node-ephem test runner\n\n');
 
-var start = false;
+rl.question('Enter a path to where the DE ephemeris files reside: ', function (path) {
+  rl.write('\n');
 
-var parse = function (line) {
-  return {
+  var files = avail(path);
 
-      'd' : parseFloat(line.substring(15,26).trim())
-    , 't' : parseInt(line.substring(26,29).trim())
-    , 'c' : parseInt(line.substring(29,32).trim())
-    , 'x' : parseInt(line.substring(32,35).trim())
-    , 'v' : parseFloat(line.substring(35,65).trim())
+  for (var i = 0; i < files.length; i++)
+    if (files[i].test)
+      rl.write((i+1) + ') ' + files[i].name + '\n');
 
-  }
-};
+  rl.question('\nEnter the number of the ephemeris file to test: ', function (number) {
+    if (files[number - 1] && files[number - 1].test != null)
+      startTest(files[number - 1]);
+    else
+      rl.write('\nInvalid selection.\n');
 
-var map   = [null, 'x', 'y', 'z', 'dx', 'dy', 'dz'];
+    rl.close();
+  });
 
-var equal = function (x, y) {
-  return Math.abs(x - y) < 2e-11;
-};
+});
 
-var stats = e430t.status();
+function startTest(de) {
 
-rd.on('line', function(line) {
-  if (!start) {
-    start = line === 'EOT';
-  } else {
-    var p = parse(line);
+  var rd = reader.createInterface({
+      'input'    : fs.createReadStream(de.test)
+    , 'output'   : process.stdout
+    , 'terminal' : false
+  });
 
-    if (p.d >= stats.start && p.d <= stats.end) {
-      var r = e430t.find(p.t, p.c, p.d);
-      assert(equal(r[map[p.x]], p.v));
-    } else {
-      console.log('Julian day out of range: ' + p.d);
+  var map      = [null, 'x', 'y', 'z', 'dx', 'dy', 'dz'];
+  var stats    = { 'run' : 0, 'errors' : 0, 'exceeded' : 0, 'start' : Date.now() };
+  var loaded   = ephem.load(de.file);
+  var meta     = loaded.status();
+  var start    = false;
+
+  function parse (line) {
+    return {
+        'day'    : parseFloat(line.substring(15,26).trim())
+      , 'target' : parseInt(line.substring(26,29).trim())
+      , 'center' : parseInt(line.substring(29,32).trim())
+      , 'var'    : map[line.substring(32,35).trim()]
+      , 'val'    : line.substring(35,65).trim()
     }
-  }
-});
+  };
+
+  function equals (value, test) {
+    var l = test.length;
+    var d = test.indexOf('.') + 1;
+    var e = 1 / Math.pow(10, l - d);
+
+    for (var i = l-1; i >= d; i--)
+      if (test.charAt(i) == '0')
+        e *= 10;
+      else
+        break;
+
+    //var p = test * 1e-14; //machine precision error
+
+    return Math.abs(value - parseFloat(test)) < Math.max(e, 5e-14);
+  };
+
+  process.stdout.write('\nRUNNING TESTS');
+
+  rd.on('line', function (line) {
+    if (!start) {
+      start = line === 'EOT';
+    } else {
+      var p = parse(line);
+
+      if (p.day >= meta.start && p.day <= meta.end) {
+        var r = loaded.find(p.target, p.center, p.day);
+
+        if (equals(r[p.var], p.val)) {
+          process.stdout.write('.');
+        } else {
+          process.stdout.write('X');
+          stats.errors++;
+        }
+
+        stats.run++;
+      } else {
+        stats.exceeded++;
+      }
+    }
+  });
+
+  rd.on('close', function () {
+    process.stdout.write('FINISHED\n\n');
+
+    if (stats.exceeded) {
+      process.stdout.write('WARNING: The test file contains ' + stats.exceeded + ' items outside the range of this ephemeris.\n\n');
+    }
+
+    process.stdout.write('Tests Complete.\n');
+    process.stdout.write('Executed ' + stats.run + ' tests in ' + (Date.now() - stats.start) + 'ms\n');
+    process.stdout.write('Errors: ' + stats.errors + '\n\n');
+
+    process.stdout.write('INFO: Some errors are likely due to machine precision issues.\n\n');
+  });
+}
