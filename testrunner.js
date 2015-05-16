efbc3088261b7021d5f57c1ead184b3437ccb739
Nenad Vukicevic <vukicevic@gmail.com>
2015-05-16 14:51:39 +0200

    Finished testrunner

--- testrunner.js
+++ testrunner.js
@@ -1,9 +1,28 @@
 var assert = require('assert');
 var reader = require('readline');
 var fs     = require('fs');
+var ph     = require('path');
 
 var ephem  = require('./index');
-var avail  = require('./available');
+
+var list = {
+    '102'  : 'lnxm1410p3002.102'
+  , '200'  : 'lnxm1600p2170.200'
+  , '202'  : 'lnxp1900p2050.202'
+  , '403'  : 'lnxp1600p2200.403'
+  , '405'  : 'lnxp1600p2200.405'
+  , '406'  : 'lnxm3000p3000.406'
+  , '410'  : 'lnxp1960p2020.410'
+  , '413'  : 'lnxp1900p2050.413'
+  , '414'  : 'lnxp1600p2200.414'
+  , '418'  : 'lnxp1900p2050.418'
+  , '421'  : 'lnxp1900p2053.421'
+  , '422'  : 'lnxm3000p3000.422'
+  , '423'  : 'lnxp1800p2200.423'
+  , '430'  : 'linux_p1550p2650.430'
+  , '430t' : 'linux_p1550p2650.430t'
+  , '431'  : 'lnxm13000p17000.431'
+};
 
 var rl = reader.createInterface({
     'input'  : process.stdin
@@ -15,7 +34,7 @@ rl.write('Welcome to the node-ephem test runner\n\n');
 rl.question('Enter a path to where the DE ephemeris files reside: ', function (path) {
   rl.write('\n');
 
-  var files = avail(path);
+  var files = available(path);
 
   for (var i = 0; i < files.length; i++)
     if (files[i].test)
@@ -31,6 +50,22 @@ rl.question('Enter a path to where the DE ephemeris files reside: ', function (p
   });
 
 });
+
+function available (path) {
+  var files = fs.readdirSync(path);
+  var avail = [];
+
+  Object.keys(list).forEach(function (de) {
+    if (files.indexOf(list[de]) >= 0)
+      avail.push({
+          'name' : 'DE' + de
+        , 'file' : path + ph.sep + list[de]
+        , 'test' : files.indexOf('testpo.' + de) >= 0 ? path + ph.sep + 'testpo.' + de : null
+      });
+  });
+
+  return avail;
+};
 
 function startTest(de) {
 
@@ -58,7 +93,7 @@ function startTest(de) {
 
   function equals (value, test) {
     var l = test.length;
-    var d = test.indexOf('.') + 1;
+    var d = test.indexOf('.') + 2;
     var e = 1 / Math.pow(10, l - d);
 
     for (var i = l-1; i >= d; i--)
@@ -67,9 +102,7 @@ function startTest(de) {
       else
         break;
 
-    //var p = test * 1e-14; //machine precision error
-
-    return Math.abs(value - parseFloat(test)) < Math.max(e, 5e-14);
+    return Math.abs(value - parseFloat(test)) < Math.max(e, 1e-14);
   };
 
   process.stdout.write('\nRUNNING TESTS');
@@ -100,14 +133,11 @@ function startTest(de) {
   rd.on('close', function () {
     process.stdout.write('FINISHED\n\n');
 
-    if (stats.exceeded) {
+    if (stats.exceeded)
       process.stdout.write('WARNING: The test file contains ' + stats.exceeded + ' items outside the range of this ephemeris.\n\n');
-    }
 
     process.stdout.write('Tests Complete.\n');
     process.stdout.write('Executed ' + stats.run + ' tests in ' + (Date.now() - stats.start) + 'ms\n');
     process.stdout.write('Errors: ' + stats.errors + '\n\n');
-
-    process.stdout.write('INFO: Some errors are likely due to machine precision issues.\n\n');
   });
 }
